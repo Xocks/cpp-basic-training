64d379a95020c93c0c82c628d44205447f3b68cf
FiLoLer Polar <141121171+Xocks@users.noreply.github.com>
2025-08-31 10:43:40 +0800

    对于字符串大的替换

---
+++ 基础第一天/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    char ar = 'a';
+        cin >> ar;
+		cout << "  " << ar << endl;
+        cout << " " << ar << ar << ar << endl;
+		cout << ar << ar << ar << ar << ar << endl;
+
+
+        return 0;
+}
